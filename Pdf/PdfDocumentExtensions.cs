44bede51a26998bc0e463df7ea05c599581ceccd
MAYUR JANSARI <mayur.jansari@gmail.com>
2022-07-19 16:26:10 +0530

    added support for .net standard 2

--- Pdf/PdfDocumentExtensions.cs
+++ Pdf/PdfDocumentExtensions.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using System.Drawing;
 
 // ReSharper disable once CheckNamespace
-namespace PdfSharp.Pdf
-{
-  /// <summary>
-  /// Extension methods for the PdfSharp library PdfItem object.
-  /// </summary>
-  public static class PdfDocumentExtensions
-  {
-    /// <summary>
-    /// Get's all of the images from the specified document.
-    /// </summary>
-    /// <param name="document">The document to extract or retrieve images from.</param>
-    /// <returns>An enumeration of images contained on the page.</returns>
-    public static IEnumerable<Image> GetImages(this PdfDocument document)
-    {
-      if (document == null) throw new ArgumentNullException("document", "The provided PDF document was null.");
+#if NET40_OR_GREATER
+namespace PdfSharp.Pdf;
+#else
+namespace PdfSharpCore.Pdf;
+#endif
 
-      foreach(PdfPage page in document.Pages) {
-        foreach(Image image in page.GetImages()) {
-          yield return (image);
-        }
-      }
+
+/// <summary>
+/// Extension methods for the PdfSharp library PdfItem object.
+/// </summary>
+public static class PdfDocumentExtensions
+{
+/// <summary>
+/// Get's all of the images from the specified document.
+/// </summary>
+/// <param name="document">The document to extract or retrieve images from.</param>
+/// <returns>An enumeration of images contained on the page.</returns>
+public static IEnumerable<Image> GetImages(this PdfDocument document)
+{
+  if (document == null) throw new ArgumentNullException("document", "The provided PDF document was null.");
+
+  foreach(PdfPage page in document.Pages) {
+    foreach(Image image in page.GetImages()) {
+      yield return (image);
     }
   }
+}
 }
--- System/Drawing/ImageExtensions.cs
+++ System/Drawing/ImageExtensions.cs
@@ -1,47 +1,54 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
+#if NET40_OR_GREATER
+namespace System.Drawing;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
-
+#else
+using PdfSharpCore.Drawing;
+using PdfSharpCore.Pdf;
+#endif
 // ReSharper disable once CheckNamespace
-namespace System.Drawing
+
+/// <summary>
+/// Extension methods for the <see cref="Image"/> class.
+/// </summary>
+public static class ImageExtensions
 {
-  /// <summary>
-  /// Extension methods for the <see cref="Image"/> class.
-  /// </summary>
-  public static class ImageExtensions
-  {
-    /// <summary>
-    /// Generates a PDF page from the image.
-    /// </summary>
-    /// <param name="image">The image to convert to a <see cref="PdfPage"/>.</param>
-    /// <returns>The <see cref="PdfPage"/> containing the image.</returns>
-    public static PdfDocument ToPdf(this Image image)
-    {
-      if (image == null) return (new PdfDocument());
-      return (ToPdf(new[] { image }));
-    }
+#if NET40_OR_GREATER
 
-    /// <summary>
-    /// Generates a PDF document from the collection of enumerations with each image representing
-    /// a new page.
-    /// </summary>
-    /// <param name="images">The collection of images to convert to pages.</param>
-    /// <returns>The <see cref="PdfDocument"/> containing the images.</returns>
-    public static PdfDocument ToPdf(this IEnumerable<Image> images)
-    {
-      PdfDocument document = new PdfDocument();
-      foreach (var image in images) {
-        PdfPage page = new PdfPage() {
-          Width = image.Width,
-          Height = image.Height
-        };
-        document.AddPage(page);
+/// <summary>
+/// Generates a PDF page from the image.
+/// </summary>
+/// <param name="image">The image to convert to a <see cref="PdfPage"/>.</param>
+/// <returns>The <see cref="PdfPage"/> containing the image.</returns>
+public static PdfDocument ToPdf(this Image image)
+{
+  if (image == null) return (new PdfDocument());
+  return (ToPdf(new[] { image }));
+}
 
-        XGraphics xGraphics = XGraphics.FromPdfPage(page);
-        XImage xImage = XImage.FromGdiPlusImage(image);
-        xGraphics.DrawImage(xImage, 0, 0, image.Width, image.Height);
-      }
-      return (document);
-    }
+/// <summary>
+/// Generates a PDF document from the collection of enumerations with each image representing
+/// a new page.
+/// </summary>
+/// <param name="images">The collection of images to convert to pages.</param>
+/// <returns>The <see cref="PdfDocument"/> containing the images.</returns>
+public static PdfDocument ToPdf(this IEnumerable<Image> images)
+{
+  PdfDocument document = new PdfDocument();
+  foreach (var image in images) {
+    PdfPage page = new PdfPage() {
+      Width = image.Width,
+      Height = image.Height
+    };
+    document.AddPage(page);
+
+    XGraphics xGraphics = XGraphics.FromPdfPage(page);
+    XImage xImage = XImage.FromGdiPlusImage(image);
+    xGraphics.DrawImage(xImage, 0, 0, image.Width, image.Height);
   }
+  return (document);
+}
+#endif
 }
